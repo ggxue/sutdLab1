8d0f0383b34302206f4965f7a69a9103166d767a
derek.xue <derek.xue@dynamitegames.io>
2023-05-27 11:46:03 +0800

    wip

---
+++ week1/lab/homework/q3/q3.py
@@ -0,0 +1,29 @@
+import hashlib
+import time
+import argparse
+
+try:
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Script description')
+    parser.add_argument('-i', '--input', type=str, help='Path to the input file', required=True)
+    parser.add_argument('-w', '--word', type=str, help='Path to the word file', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Path to the output file', required=True)
+
+    args = parser.parse_args()
+    inputs = open(args.input).read().splitlines()
+    words = open(args.word).read().splitlines()
+    output = open(args.output, 'w')
+
+    cracked_count = 0
+    for h in inputs:
+        for w in words:
+            if h == hashlib.md5(w.encode()).hexdigest():
+                output.write('{} : {}\n'.format(w, h))
+                cracked_count += 1
+                
+    end_time = time.time()
+    output.write('Dictionary attack completed! Cracked {} out of {}, time taken {}s\n'.format(cracked_count, len(inputs), end_time - start_time))
+
+except Exception as e:
+    print("An unexpected error occurred:", str(e))
